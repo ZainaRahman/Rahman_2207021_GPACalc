29a668b4db91f85d2a00ac5011e1591ca8d109b7
Zaina Rahman <rahman2207021@stud.kuet.ac.bd>
2025-11-15 12:49:47 +0100

    Fixed the size to fit in the window

--- src/main/java/com/example/gpacalc/HelloApplication.java
+++ src/main/java/com/example/gpacalc/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.gpacalc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root=loader.load();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setResizable(false);
+        stage.setWidth(600);
+        stage.setHeight(600);
+        stage.setTitle("Home Page");
         stage.setScene(scene);
         stage.show();
     }
