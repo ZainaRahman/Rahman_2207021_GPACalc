efa749c0da2de4c27cb017a3bbf55b8cbb280def
Zaina Rahman <rahman2207021@stud.kuet.ac.bd>
2025-11-15 12:52:15 +0100

    java graphics required

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,8 @@ module com.example.gpacalc {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires javafx.graphics;
+    requires javafx.base;
 
     opens com.example.gpacalc to javafx.fxml;
     exports com.example.gpacalc;
