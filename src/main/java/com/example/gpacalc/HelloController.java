f55c669ac84b987a7ebbf54616da28dd3b47226a
Zaina Rahman <rahman2207021@stud.kuet.ac.bd>
2025-11-15 12:51:43 +0100

    Added the scene changing to homepage navigation

--- src/main/java/com/example/gpacalc/HelloController.java
+++ src/main/java/com/example/gpacalc/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.gpacalc;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void swtichToScene2(ActionEvent event)throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene2.fxml"));
+        Parent root = loader.load();
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setTitle("Calculator!!");
+        stage.setScene(scene);
+        stage.show();
     }
+
+
+
 }
