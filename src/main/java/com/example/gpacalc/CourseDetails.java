ef1809a611f02bb37b641f19804ddc89d1ab34ac
Zaina Rahman <rahman2207021@stud.kuet.ac.bd>
2025-11-17 04:27:14 +0600

    details updated to add teachers name

--- src/main/java/com/example/gpacalc/CourseDetails.java
+++ src/main/java/com/example/gpacalc/CourseDetails.java
@@ -1,21 +1,30 @@
 package com.example.gpacalc;
 
 import javafx.beans.property.*;
+import javafx.scene.control.TextField;
 
 public class CourseDetails {
     private final StringProperty name;
     private final DoubleProperty credit;
     private final StringProperty grade;
     private final StringProperty code;
+    private final StringProperty teacher1;
+    private final StringProperty teacher2;
 
 
-    public CourseDetails(String name,Double credit,String grade,String code) {
+    public CourseDetails(String name, Double credit, String grade, String code, String teacher1, String teacher2) {
         this.name = new SimpleStringProperty(name);
         this.credit =new SimpleDoubleProperty(credit);
         this.grade = new SimpleStringProperty(grade);
         this.code = new SimpleStringProperty(code);
+        this.teacher1 = new SimpleStringProperty(teacher1);
+        this.teacher2 = new SimpleStringProperty(teacher2);
+
 
     }
+
+
+
     public String getName() {
         return name.get();
     }
@@ -28,6 +37,14 @@ public class CourseDetails {
     public String getCode() {
         return code.get();
     }
+    public String getTeacher1() {
+        return teacher1.get();
+    }
+    public String getTeacher2(){
+        return teacher2.get();
+    }
+
+
 
     public StringProperty nameProperty() {
         return name;
@@ -41,6 +58,13 @@ public class CourseDetails {
     public StringProperty codeProperty() {
         return code;
     }
+    public StringProperty T1Property() {
+        return teacher1;
+    }
+    public StringProperty T2Property() {
+        return teacher2;
+    }
+
 }
 
 
